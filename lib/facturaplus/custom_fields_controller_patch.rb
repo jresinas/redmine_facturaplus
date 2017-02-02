b8d71ba0694521411f47aab6a7cc2e4c94ac9ebf
jresinas <jresinas@emergya.com>
2017-02-02 20:49:28 +0100

    Allow to set a list of default clients

--- lib/facturaplus/custom_fields_controller_patch.rb
+++ lib/facturaplus/custom_fields_controller_patch.rb
@@ -22,8 +22,8 @@ module Facturaplus
 				  	res = Facturaplus::Fp.get_clients(client_field_id)
 
 				  	if res[:result]
-				  		client_field.possible_values=res[:options]
-						render :text => {:type => 'success', :message => l(:'facturaplus.text_sync_success'), :data => res[:options]}.to_json and return if client_field.save
+				  		client_field.possible_values = (Setting.plugin_redmine_facturaplus['default_clients'].split("\r\n") + res[:options])
+						render :text => {:type => 'success', :message => l(:'facturaplus.text_sync_success'), :data => client_field.possible_values}.to_json and return if client_field.save
 					end
 				end
 				render :text => {:type => 'error', :message => l(:'facturaplus.text_sync_fail')}.to_json
