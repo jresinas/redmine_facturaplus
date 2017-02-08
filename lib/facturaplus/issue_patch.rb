366ce9f8308f5200dc2d16a1b1fdfef9a573a1f6
jresinas <jresinas@emergya.com>
2017-02-08 17:28:59 +0100

    Update integration flow

--- lib/facturaplus/issue_patch.rb
+++ lib/facturaplus/issue_patch.rb
@@ -44,8 +44,16 @@ module Facturaplus
         amount_field = self.custom_values.find_by(custom_field: Setting.plugin_redmine_facturaplus['amount_field'])
         currency_field = self.custom_values.find_by(custom_field: Setting.plugin_redmine_facturaplus['currency_field'])
 
-        if tracker_id.to_s == Setting.plugin_redmine_facturaplus['bill_tracker'] and !Setting.plugin_redmine_facturaplus['billed_statuses'].include?(status_id.to_s) and biller_field.present? and client_field.present? and amount_field.present? and currency_field.present? and Setting.plugin_redmine_facturaplus['billers'].include?(biller_field.value)
-          # Es una factura emitada por una empresa con FacturaPlus
+        if (!biller_field.present? or !client_field.present? or !amount_field.present? or !currency_field.present?) and Setting.plugin_redmine_facturaplus['billers'].include?(biller_field.value)
+          # Faltan campos requeridos en el ticket
+          errors[:base] << I18n.t('facturaplus.text_fields_missing')
+          # Patch to fix error when try to submit a NEW issue form after Rollback in facturaplus_bill_save method
+          self.destroy if @new_bill.present?
+          raise ActiveRecord::Rollback
+        end
+
+        if tracker_id.to_s == Setting.plugin_redmine_facturaplus['bill_tracker'] and !Setting.plugin_redmine_facturaplus['billed_statuses'].include?(status_id.to_s) and Setting.plugin_redmine_facturaplus['billers'].include?(biller_field.value)
+          # Es una factura emitada por una empresa con FacturaPlus y está en estado NO facturado
           biller_id = begin Facturaplus::BILLER_IDS[biller_field.value] rescue nil end
           client_id = begin FacturaplusClient.find_by(client_name: client_field.value, biller_id: biller_id).client_id rescue nil end
           amount = begin amount_field.value.to_f rescue nil end
@@ -76,8 +84,8 @@ module Facturaplus
             results += self.destroy_delivery_note
           end
         elsif self.facturaplus_relation.present?
-          # No es una factura emitada por una empresa con FacturaPlus, pero tiene elementos de FacturaPlus asociados -> borramos los elementos asociados
-          results += self.destroy_order
+          # No es una factura emitada por una empresa con FacturaPlus, pero tiene elementos de FacturaPlus asociados -> borramos los elementos asociados si NO está en estado facturado o NO está emitido por una empresa con FacturaPlus       
+          results += self.destroy_order if !Setting.plugin_redmine_facturaplus['billed_statuses'].include?(status_id.to_s) or !Setting.plugin_redmine_facturaplus['billers'].include?(biller_field.value)
         end
 
         if (res = results.flatten.find{|r| r[:result].blank?})
